2b136c9721a1e08ab6f183943bb94cd17d2b0191
Julia Miroshnichenko22 <jumelie@yandex.ru>
2023-09-03 20:14:07 +0300

    mobBottombarAdd

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,4 +1,4 @@
-import { SignedIn } from '@clerk/nextjs';
+import { OrganizationSwitcher, SignOutButton, SignedIn } from '@clerk/nextjs';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -11,8 +11,17 @@ function Topbar() {
       </Link>
       <div className="flex items-center gap-1">
         <div className="block md:hidden">
-          <SignedIn></SignedIn>
+          <SignedIn>
+            <SignOutButton>
+              <div className="flex cursor-pointer">
+                <Image src="/assets/logout.svg" alt="logout" width={24} height={24} />
+              </div>
+            </SignOutButton>
+          </SignedIn>
         </div>
+        <OrganizationSwitcher
+          appearance={{ elements: { organizationSwitcherTrigger: 'py-2 px-4' } }}
+        />
       </div>
     </nav>
   );
